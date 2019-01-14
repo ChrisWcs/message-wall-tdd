96d95b84db5996862617a476fddefcb8e2c3c521
Christian Hansen <ChrisWcs@users.noreply.github.com>
2019-01-14 14:25:17 -0600

    Added first test and got test to pass

--- src/App.js
+++ src/App.js
@@ -1,28 +1,24 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+class App extends React.Component {
+    state = {
+        value: ''
+    };
+
+    handleChange = (event) => {
+        this.setState({
+            value: event.target.value
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <input value={this.state.value} data-testid="input" onChange={this.handleChange}/>
+                <button data-testid="post">Post</button>
+            </div>
+        );
+    }
 }
 
 export default App;
---
+++ src/App.spec.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import { render, fireEvent, cleanup } from 'react-testing-library'
+import App from './App';
+
+test('the app contains a form I can fill with text', () => {
+    const {getByTestId} = render(<App />);
+    const my_input = getByTestId('input');
+    expect(my_input.value).toEqual("");
+
+    fireEvent.change(my_input, { 
+        target: { 
+            value: "some words"
+        } 
+    });
+    
+    expect(my_input.value).toEqual("some words");
+    cleanup();
+});
+
+test('the app contains a button that will post the input to the wall', () => {
+    const {getByTestId} = render(<App />);
+    const my_input = getByTestId('input');
+    const post_button = getByTestId('post');
+
+    fireEvent.change(my_input, { 
+        target: { 
+            value: "some words"
+        } 
+    });
+
+    fireEvent.click(post_button);
+    
+    expect(my_input.value).toEqual("");
+    expect(getByTestId('post-1').textContent).toEqual("some words");
+    cleanup();
+});
